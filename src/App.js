a63844b29961cb822583b6e8e56f2bf2ae066d84
Melford-D <melfordbirakor@gmail.com>
2022-06-10 23:36:33 +0100

    added moralis SDK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Moralis from "moralis";
+import { useState } from "react";
+import { NftExplore } from "./pages/NftExplore";
 
 function App() {
+  const userCurrentTheme = JSON.parse(localStorage.getItem("isDark"));
+  const [isDark, setisDark] = useState(userCurrentTheme || true);
+
+  // initMoralis function
+  const initMoralis = async () => {
+    // connect to Moralis server
+    await Moralis.start({
+      serverUrl: process.env.REACT_APP_SERVER_URL,
+      appId: process.env.REACT_APP_APP_ID,
+    });
+  };
+
+  // call/invoke initMoralis function
+  initMoralis();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-red-900">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={isDark ? "dark" : ""}>
+      <div className="dark:bg-gray-800 pt-10 px-8 bg-gray-50">
+        <section className="container mx-auto w-full max-w-screen-xl min-h-screen">
+          <NftExplore settheme={setisDark} />
+        </section>
+      </div>
+    </main>
   );
 }
 
---
+++ src/pages/NftExplore.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const NftExplore = () => {
+  return <div className="text-white">NftExplore</div>;
+};
